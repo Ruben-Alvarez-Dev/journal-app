47eeacfa12a74418c183596d456e5d53e752adac
ruben <ruben@imac.local>
2024-02-27 18:14:00 +0100

    congigureStore from toolkit, not createStore from redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
-import { combineReducers, createStore } from "redux";
+import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from "../reducers/authReducer";
 
-const reducers = combineReducers({
-    auth: authReducer
-})
-export const store = createStore(reducers)
+ export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+    },
+});
