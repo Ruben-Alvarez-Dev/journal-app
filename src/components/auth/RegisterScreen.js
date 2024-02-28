3f726fe49147bac64f99ad4cf4afa369c2dde802
ruben <ruben@imac.local>
2024-02-28 16:18:17 +0100

    handling input errors

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,26 +1,62 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import validator from "validator";
+
 import { useForm } from "../../hooks/useForm";
+
+import { useDispatch } from "react-redux";
+import { setError, removeError } from "../../actions/ui";
 
 
 export const RegisterScreen = () => {
     
+    const dispatch = useDispatch();
+    
     const [formValues, handleInputChange ] = useForm({
         name: "Ruben",
         email: "hola@yo.com",
-        password: "123",
-        password2: "123"
+        password: "123456",
+        password2: "123456"
     })
     
     const { name, email, password, password2 } = formValues;
     
     const handleRegister = (e) => {
         e.preventDefault();
-            }
+        if ( isFormValid() ) {
+            console.log("Formulario correcto");
+        }
+    }
+
+    const isFormValid = () => {
+        
+        if ( name.trim().length === 0 ) {
+            dispatch( setError("Name is required") );
+            
+            return false;
+
+        } else if ( !validator.isEmail( email ) ) {
+            dispatch( setError("Email is not valid") );
+            return false;
+
+        } else if ( password !== password2 || password.length < 5 ) {
+            dispatch( setError("Incomplete or too shor password") );
+            return false;
+        }
+        dispatch( removeError() );        
+        return true
+    }
+
+
     return (
       <div>
            <h3 className="auth__title">Register</h3>
         
         <form onSubmit={ handleRegister }>
+            
+            <div className="auth__alert-error">
+                <p>ERROR</p>
+            </div>
             
             <input 
                 className="auth__input"
