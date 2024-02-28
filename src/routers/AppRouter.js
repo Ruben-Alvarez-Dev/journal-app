7774aa76a8b529cbd9f317375bc957895cf04f9f
ruben <ruben@imac.local>
2024-02-28 18:12:25 +0100

    firebase observable object

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournalScreen';
+/* import firebase from 'firebase'; // Import the 'firebase' package */
+import { firebase } from "../firebase/firebase-config"
+import { useDispatch } from 'react-redux'; // Import the 'useDispatch' hook from the Redux store
+import { login } from '../actions/auth'; // Import the 'login' action from the Redux store
 
 export const AppRouter = () => {
+    
+    const dispatch = useDispatch(); // Initialize the 'dispatch' function
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user?.uid) {
+                dispatch(login(user.uid, user.displayName));
+            }
+        });
+    }, []); // Add the missing closing parenthesis for the 'useEffect' hook
+
     return (
         <Router>
             <div>
