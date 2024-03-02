dd4694f2703faf379c48bda88e995f6550078250
ruben <ruben@imac.local>
2024-03-02 14:36:52 +0100

    added Swal alert

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,15 +1,32 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { NotesAppBar } from "./NotesAppBar"
 import { useForm } from "../../hooks/useForm"
+import { useEffect, useRef } from "react";
+import { activeNote } from "../../actions/notes";
+
 
 export const NoteScreen = () => {
-  
+
+  const dispatch = useDispatch();
   const { active:note } = useSelector ( state => state.notes );
-
-  const [ formValues, handleInputChange ] = useForm( note );
-
-  console.log(formValues);
+  const [ formValues, handleInputChange, reset ] = useForm( note );
   const { body, title } = formValues;
+
+  const activeId = useRef( note.id );
+
+  useEffect(() => {
+
+    if ( note.id !== activeId.current ) {
+        reset( note );
+        activeId.current = note.id;
+    }
+
+  }, [note, reset]);
+
+  useEffect(() => {
+    dispatch( activeNote(formValues.id, {...formValues}) );
+  }
+  , [formValues, dispatch]);
 
   return (
     <div className="notes__main-content">
@@ -23,14 +40,16 @@ export const NoteScreen = () => {
               placeholder="Some awesome title" 
               className="notes__title-input"
               autoComplete="off"
-              value= { body }
+              name="title"
+              value= { title }
               onChange={ handleInputChange }
           />
 
           <textarea
               placeholder="What happened today"
               className="notes__textarea"
-              value= { title }
+              name="body"
+              value= { body }
               onChange={ handleInputChange }
           ></textarea>
 
